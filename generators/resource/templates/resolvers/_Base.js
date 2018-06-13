0a378807b89b4226321ced4d2844e8d41cb8696c
Zhang Kaiyu <yeannylam@gmail.com>
2018-06-13 18:34:59 +0800

    Add resolver template

---
+++ generators/resource/templates/resolvers/_Base.js
@@ -0,0 +1,26 @@
+module.exports = {
+  Query: {
+    async <%- svarName %>(root, { _id }, ctx) {
+      const { <%- modelName %> } = ctx.models;
+      return await <%- modelName %>.findById(_id);
+    },
+    async <%- pvarName %>(root, { _ }, ctx) {
+      const { <%- modelName %> } = ctx.models;
+      return await <%- modelName %>.find();
+    }
+  },
+  Mutation: {
+    async create<%- modelName %>(root, { input }, ctx) {
+      const { <%- modelName %> } = ctx.models;
+      return await <%- modelName %>.create(input);
+    },
+    async update<%- modelName %>(root, { _id, input }, ctx) {
+      const { <%- modelName %> } = ctx.models;
+      return await <%- modelName %>.findOneAndUpdate({ _id }, input, { new: true });
+    },
+    async delete<%- modelName %>(root, { _id }, ctx) {
+      const { <%- modelName %> } = ctx.models;
+      return await <%- modelName %>.findByIdAndDelete(_id);
+    }
+  }
+};
