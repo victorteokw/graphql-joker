e2e508f561b47a2b174de2dcb203d8e9fff3c62b
Zhang Kaiyu <yeannylam@gmail.com>
2018-06-21 20:24:36 +0800

    better test suite

--- __tests__/app.js
+++ __tests__/app.js
@@ -13,22 +13,44 @@ describe('yo amur:app brand-new-app', () => {
       .withPrompts({ name: 'brand-new-app' });
     return context;
   });
+  const expectedDir = path.join(__dirname, 'expected/brand-new-app');
+  const filesAndDirs = glob.sync(expectedDir + '/**/*');
 
-  it('creates files and directories', () => {
-    const expectedDir = path.join(__dirname, 'expected/brand-new-app');
-    const filesAndDirs = glob.sync(expectedDir + '/**/*');
-    filesAndDirs.forEach((f) => {
-      if (fs.lstatSync(f).isDirectory()) {
+  filesAndDirs.forEach((f) => {
+    if (fs.lstatSync(f).isDirectory()) {
+      it(`create directory ${path.relative(expectedDir, f)}`, () => {
         assert(fs.lstatSync(path.join(context.targetDirectory, path.relative(expectedDir, f))).isDirectory());
-      } else {
-        if (path.relative(expectedDir, f) === 'package.json') {
+      });
+    } else {
+      if (path.relative(expectedDir, f) === 'package.json') {
+        it('has name field', () => {
           const fileContents = fs.readFileSync(path.join(context.targetDirectory, path.relative(expectedDir, f))).toString();
           const j = JSON.parse(fileContents);
           assert(j.name === 'brand-new-app');
+        });
+        it('has title field', () => {
+          const fileContents = fs.readFileSync(path.join(context.targetDirectory, path.relative(expectedDir, f))).toString();
+          const j = JSON.parse(fileContents);
           assert(j.title === 'brand-new-app');
+        });
+        it('version is 0.0.1', () => {
+          const fileContents = fs.readFileSync(path.join(context.targetDirectory, path.relative(expectedDir, f))).toString();
+          const j = JSON.parse(fileContents);
           assert(j.version === '0.0.1');
+        });
+        it('should be a private project', () => {
+          const fileContents = fs.readFileSync(path.join(context.targetDirectory, path.relative(expectedDir, f))).toString();
+          const j = JSON.parse(fileContents);
           assert(j.private === true);
+        });
+        it('has a main file called app.js', () => {
+          const fileContents = fs.readFileSync(path.join(context.targetDirectory, path.relative(expectedDir, f))).toString();
+          const j = JSON.parse(fileContents);
           assert(j.main === 'app.js');
+        });
+        it('sets correct scripts', () => {
+          const fileContents = fs.readFileSync(path.join(context.targetDirectory, path.relative(expectedDir, f))).toString();
+          const j = JSON.parse(fileContents);
           assert.deepEqual(j.scripts, {
             "start": "nodemon app.js",
             "console": "dobukulbira",
@@ -36,25 +58,35 @@ describe('yo amur:app brand-new-app', () => {
             "seed": "nonula seed",
             "drop": "nonula drop"
           });
-
-          [
-            'koa', 'mongoose', 'graphql',
-            'koa-body', 'koa-logger', 'koa-router', 'koa2-mongoose', '@koa/cors',
-            'graphql-tools', 'merge-graphql-schemas', 'apollo-server-koa',
-            'noenv', 'lodash', 'glob'
-          ].forEach((name) => {
+        });
+        [
+          'koa', 'mongoose', 'graphql',
+          'koa-body', 'koa-logger', 'koa-router', 'koa2-mongoose', '@koa/cors',
+          'graphql-tools', 'merge-graphql-schemas', 'apollo-server-koa',
+          'noenv', 'lodash', 'glob'
+        ].forEach((name) => {
+          it(`installs the dependency ${name}`, () => {
+            const fileContents = fs.readFileSync(path.join(context.targetDirectory, path.relative(expectedDir, f))).toString();
+            const j = JSON.parse(fileContents);
             assert(!!j.dependencies[name]);
           });
-
-          [
-            'eslint', 'nodemon', 'nonula', 'dobukulbira', 'jest', 'jest-cli'
-          ].forEach((name) => {
+        });
+        [
+          'eslint', 'nodemon', 'nonula', 'dobukulbira', 'jest', 'jest-cli'
+        ].forEach((name) => {
+          it(`installs the dev dependency ${name}`, () => {
+            const fileContents = fs.readFileSync(path.join(context.targetDirectory, path.relative(expectedDir, f))).toString();
+            const j = JSON.parse(fileContents);
             assert(!!j.devDependencies[name]);
           });
-        } else {
+        });
+      } else {
+        it(`create file ${path.relative(expectedDir, f)} with correct content.`, () => {
           assert.fileContent(path.relative(expectedDir, f), fs.readFileSync(f).toString());
-        }
+        });
+
       }
-    });
+    }
   });
+
 });
