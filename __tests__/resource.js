aa300ea04f4301617930fa46a1185846d39a8253
Zhang Kaiyu <yeannylam@gmail.com>
2018-06-21 19:31:50 +0800

    fix test

--- __tests__/resource.js
+++ __tests__/resource.js
@@ -266,18 +266,18 @@ describe('yo amur:resource', () => {
     });
 
     it('create correct model file', () => {
-      const c = fs.readFileSync(path.join(dir, 'models/Article.js')).toString();
-      assert.fileContent('models/Article.js', c);
+      const c = fs.readFileSync(path.join(dir, 'models/User.js')).toString();
+      assert.fileContent('models/User.js', c);
     });
 
     it('create correct schema file', () => {
-      const c = fs.readFileSync(path.join(dir, 'schemas/Article.gql')).toString();
-      assert.fileContent('schemas/Article.gql', c);
+      const c = fs.readFileSync(path.join(dir, 'schemas/User.gql')).toString();
+      assert.fileContent('schemas/User.gql', c);
     });
 
     it('create correct resolver file', () => {
-      const c = fs.readFileSync(path.join(dir, 'resolvers/Article.js')).toString();
-      assert.fileContent('resolvers/Article.js', c);
+      const c = fs.readFileSync(path.join(dir, 'resolvers/User.js')).toString();
+      assert.fileContent('resolvers/User.js', c);
     });
   });
 });
