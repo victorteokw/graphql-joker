552f2758d8c53206388d34e8a9b3d05ca3e5674d
Zhang Kaiyu <yeannylam@gmail.com>
2018-06-22 15:43:48 +0800

    more model desc tests

--- __tests__/modelDescriptor.js
+++ __tests__/modelDescriptor.js
@@ -133,4 +133,292 @@ describe('model descriptor', () => {
       }
     });
   });
+
+  it('handles arrays', () => {
+    const desc = modelDescriptor(['User', 'name:[String]!^$:No Name', 'posts:[Post]']);
+    assert.deepEqual(desc, {
+      "modelName": "User",
+      "collectionName": "users",
+      "varName": "user",
+      "pluralVarName": "users",
+      "fields": [
+        {
+          "name": "name",
+          "type": "String",
+          "jsType": "String",
+          "graphQLType": "String",
+          "isArray": true,
+          "primitive": true,
+          "modifiers": {
+            "required": true,
+            "index": true,
+            "unique": true,
+            "default": "\"No Name\""
+          },
+          foreignKey: undefined,
+          foreignKeyIsArray: undefined
+        },
+        {
+          "name": "posts",
+          "type": "Post",
+          "jsType": "Post",
+          "graphQLType": "Post",
+          "isArray": true,
+          "primitive": false,
+          "modifiers": {},
+          foreignKey: undefined,
+          foreignKeyIsArray: undefined
+        }
+      ],
+      "sideEffects": {
+        "requiresObjectId": true,
+        "requiresDate": false,
+        "needsResolverModelBody": true
+      }
+    });
+  });
+
+  it('handle foreign keys', () => {
+    const desc = modelDescriptor(['User', 'posts:[Post]:author']);
+    assert.deepEqual(desc, {
+      "modelName": "User",
+      "collectionName": "users",
+      "varName": "user",
+      "pluralVarName": "users",
+      "fields": [
+        {
+          "name": "posts",
+          "type": "Post",
+          "jsType": "Post",
+          "graphQLType": "Post",
+          "isArray": true,
+          "primitive": false,
+          "modifiers": {},
+          "foreignKey": "author",
+          foreignKeyIsArray: undefined
+        }
+      ],
+      "sideEffects": {
+        "requiresObjectId": false,
+        "requiresDate": false,
+        "needsResolverModelBody": true
+      }
+    });
+  });
+
+  it('handle default value', () => {
+    const desc = modelDescriptor(['User', 'age:Number:18']);
+    assert.deepEqual(desc, {
+      "modelName": "User",
+      "collectionName": "users",
+      "varName": "user",
+      "pluralVarName": "users",
+      "fields": [
+        {
+          "name": "age",
+          "type": "Number",
+          "jsType": "Number",
+          "graphQLType": "Int",
+          "isArray": false,
+          "primitive": true,
+          "modifiers": {
+            "default": 18
+          },
+          foreignKey: undefined,
+          foreignKeyIsArray: undefined
+        }
+      ],
+      "sideEffects": {
+        "requiresObjectId": false,
+        "requiresDate": false,
+        "needsResolverModelBody": false
+      }
+    });
+  });
+
+  it('handle index modifier', () => {
+    const desc = modelDescriptor(['User', 'name:String^']);
+    assert.deepEqual(desc, {
+      "modelName": "User",
+      "collectionName": "users",
+      "varName": "user",
+      "pluralVarName": "users",
+      "fields": [
+        {
+          "name": "name",
+          "type": "String",
+          "jsType": "String",
+          "graphQLType": "String",
+          "isArray": false,
+          "primitive": true,
+          "modifiers": {
+            "index": true
+          },
+          foreignKey: undefined,
+          foreignKeyIsArray: undefined
+        }
+      ],
+      "sideEffects": {
+        "requiresObjectId": false,
+        "requiresDate": false,
+        "needsResolverModelBody": false
+      }
+    });
+  });
+
+  it('handle required modifier', () => {
+    const desc = modelDescriptor(['User', 'name:String!']);
+    assert.deepEqual(desc, {
+      "modelName": "User",
+      "collectionName": "users",
+      "varName": "user",
+      "pluralVarName": "users",
+      "fields": [
+        {
+          "name": "name",
+          "type": "String",
+          "jsType": "String",
+          "graphQLType": "String",
+          "isArray": false,
+          "primitive": true,
+          "modifiers": {
+            "required": true
+          },
+          foreignKey: undefined,
+          foreignKeyIsArray: undefined
+        }
+      ],
+      "sideEffects": {
+        "requiresObjectId": false,
+        "requiresDate": false,
+        "needsResolverModelBody": false
+      }
+    });
+
+  });
+
+  it('handle unique modifier', () => {
+    const desc = modelDescriptor(['User', 'name:String$']);
+    assert.deepEqual(desc, {
+      "modelName": "User",
+      "collectionName": "users",
+      "varName": "user",
+      "pluralVarName": "users",
+      "fields": [
+        {
+          "name": "name",
+          "type": "String",
+          "jsType": "String",
+          "graphQLType": "String",
+          "isArray": false,
+          "primitive": true,
+          "modifiers": {
+            "unique": true
+          },
+          foreignKey: undefined,
+          foreignKeyIsArray: undefined
+        }
+      ],
+      "sideEffects": {
+        "requiresObjectId": false,
+        "requiresDate": false,
+        "needsResolverModelBody": false
+      }
+    });
+  });
+
+  it('handle combined modifiers', () => {
+    const desc = modelDescriptor(['User', 'name:String$^!']);
+    assert.deepEqual(desc, {
+      "modelName": "User",
+      "collectionName": "users",
+      "varName": "user",
+      "pluralVarName": "users",
+      "fields": [
+        {
+          "name": "name",
+          "type": "String",
+          "jsType": "String",
+          "graphQLType": "String",
+          "isArray": false,
+          "primitive": true,
+          "modifiers": {
+            "required": true,
+            "index": true,
+            "unique": true
+          },
+          foreignKey: undefined,
+          foreignKeyIsArray: undefined
+        }
+      ],
+      "sideEffects": {
+        "requiresObjectId": false,
+        "requiresDate": false,
+        "needsResolverModelBody": false
+      }
+    });
+  });
+
+  it('handle string match modifiers', () => {
+    const desc = modelDescriptor(['User', 'name:String/\\w+/']);
+    assert.deepEqual(desc, {
+      "modelName": "User",
+      "collectionName": "users",
+      "varName": "user",
+      "pluralVarName": "users",
+      "fields": [
+        {
+          "name": "name",
+          "type": "String",
+          "jsType": "String",
+          "graphQLType": "String",
+          "isArray": false,
+          "primitive": true,
+          "modifiers": {
+            match: '/\\w+/'
+          },
+          foreignKey: undefined,
+          foreignKeyIsArray: undefined
+        }
+      ],
+      "sideEffects": {
+        "requiresObjectId": false,
+        "requiresDate": false,
+        "needsResolverModelBody": false
+      }
+    });
+  });
+
+  it('string match modifiers should come before other modifiers', () => {
+    const desc = modelDescriptor(['User', 'name:String/\\w+/!^$']);
+    assert.deepEqual(desc, {
+      "modelName": "User",
+      "collectionName": "users",
+      "varName": "user",
+      "pluralVarName": "users",
+      "fields": [
+        {
+          "name": "name",
+          "type": "String",
+          "jsType": "String",
+          "graphQLType": "String",
+          "isArray": false,
+          "primitive": true,
+          "modifiers": {
+            match: '/\\w+/',
+            required: true,
+            index: true,
+            unique: true
+          },
+          foreignKey: undefined,
+          foreignKeyIsArray: undefined
+        }
+      ],
+      "sideEffects": {
+        "requiresObjectId": false,
+        "requiresDate": false,
+        "needsResolverModelBody": false
+      }
+    });
+  });
 });
