9f1175595628b72a192a78f51c2410447b9cd16c
Zhang Qingyu <zqy9699@gmail.com>
2018-06-30 00:52:03 +0800

    #19 help message for amur

--- cli/help.js
+++ cli/help.js
@@ -3,9 +3,14 @@ module.exports = (command) => {
   if (command) {
     msg = `help for '${command}'`;
   } else {
-    msg = 'help for amur';
+    msg = '\nUsage: amur <command> [args...]\
+    \n\nCommands:\
+    \n\n\tapp\t\tCreate a brand new app.\
+    \n\n\tresource\tGenerate a set of API resource.\
+    \n\n\tschema\t\tGenerate a reusable schema for API resouces.\
+    \n';
   }
-  
+
   process.stdout.write(msg + '\n');
   process.exit(0);
 };
