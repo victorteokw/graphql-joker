b29cee15befa78caf4185671cc20667eeccf3d19
Zhang Kaiyu <yeannylam@gmail.com>
2018-09-26 15:03:56 +0800

    update test

--- tests/suites/commands/expected/brand-new-app/server.js
+++ tests/suites/commands/expected/brand-new-app/server.js
@@ -1,31 +1,29 @@
 const config = require('noenv');
 
-const path = require('path');
-const fs = require('fs');
-const glob = require('glob');
-
 const mongoose = require('mongoose');
 const mongooseUploader = require('mongoose-uploader');
-mongoose.Types.ObjectId.prototype.valueOf = function () {
-  return this.toString();
-};
 
 const { ApolloServer, makeExecutableSchema } = require('apollo-server');
 const { mergeTypes } = require('merge-graphql-schemas');
 const { applyMiddleware } = require('graphql-middleware');
 
+const path = require('path');
+const fs = require('fs');
+const glob = require('glob');
 const merge = require('lodash.merge');
 const map = require('lodash.map');
 
 const middlewares = require('./middlewares');
 
+mongoose.Types.ObjectId.prototype.valueOf = function () {
+  return this.toString();
+};
+mongoose.plugin(mongooseUploader);
 const modelDir = path.join(__dirname, 'models');
+map(glob.sync(path.join(modelDir, '**/*.js')), require);
+
 const schemaDir = path.join(__dirname, '/schemas');
 const resolverDir = path.join(__dirname, '/resolvers');
-
-mongoose.plugin(mongooseUploader);
-
-map(glob.sync(path.join(modelDir, '**/*.js')), require);
 
 const schema = makeExecutableSchema({
   typeDefs: mergeTypes(map(
